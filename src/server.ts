02425f0fdd9a51849ea68695b3d8800919ce1407
David Alonso <davidealonso.da@gmail.com>
2021-07-27 19:30:43 -0300

    Fix: Clear comments

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,3 @@
-
-// var express = require('express')
-// var app = express()
-
-// app.get('/', function (req: Request, res: Response) {
-//   res.send('Api works!')
-// })
-
-// app.listen(3003, function () {
-//   console.log('app listening on port 3003')
-// })
 import 'reflect-metadata';
 import App from './App';
 import express, { Application } from 'express';
@@ -28,11 +17,6 @@ class Server {
   }
 
   private up(port: number) {
-    /*
-    Start Express Project on a specific PORT
-    # If you don't put "no-console" : false in tslint.json
-    # TSLint will prevent this line and throw an error.
-    */
     this.express.listen(port, () => {
       console.log('Express Application listening on port ' + port);
     });
